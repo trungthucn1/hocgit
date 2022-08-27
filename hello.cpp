c0c5f2d38202cded794b4813664a27f58aa3150a
trungthucn1 <trungthucn2000@gmail.com>
2022-08-27 20:35:20 +0700

    aaaa

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,9 @@
 #include<iostream>
-
+#include"source.cpp"
 using namespace std;
 
 int main()
 {
-	int a;
-	int *p ;
-	a = 10 ;
-	p = &a;
-	cout<< p << " " <<   *p << endl;
-	int b = 20;
-	*p = b ;
-	cout<< p <<"  "<<   *p << endl;
+	People thuc1;
+	return 0;
 }
