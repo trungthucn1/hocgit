341500e4dfcb5ae2f3e22af2d10903186dac58e4
trungthucn1 <trungthucn2000@gmail.com>
2022-08-27 09:44:22 +0700

    sua lai

--- hello.cpp
+++ hello.cpp
@@ -5,11 +5,9 @@ using namespace std;
 int main()
 {
 	int n;
-<<<<<<< HEAD
+
 	cout<<"Hello 11euuuueee";
-=======
-	cout<<"Hello 11eeeeqqqq";
->>>>>>> c3c223710004e5c4104319c2ce7268623b411d21
+
 	cout<<"2222";
 	cin>>n;
 	cout<<n;
